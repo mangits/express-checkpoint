c319451a71f0369f80a63020b378db9c2977cdd4
Mark <mterry.gazelle@gmail.com>
2021-06-25 17:50:09 +0200

    updates

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,50 @@
 var express = require('express');
 var router = express.Router();
 const movies = require('../movies.json')
+const fs = require('fs')
 
 
 router.get('/', (req, res, next) => {
-    res.send(movies)
+    let queryTitle = req.query.title
+    if (queryTitle) {
+        let queryResults = movies.filter(movie => movie.title.toLowerCase().includes(queryTitle))
+        if (queryResults) {
+            res.send(queryResults)
+        } else {
+            res.status(404).send('No matching movies')
+        }
+    } else {
+        res.send(movies)
+    }
+}) //working
+
+router.get('/:id', (req, res, next) => {
+    let id = req.params.id
+    let filteredMovies = movies.filter(movie => movie.id == id)
+    res.send(filteredMovies)
+}) //working
+
+router.post('/', (req, res, next) => {
+
+    movies.push()
+    fs.writeFile('movies.json', JSON.stringify(moviesArray), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    })
 })
+
+router.delete('/:id', (req, res) => {
+    let movieID = Number(req.params.id)
+    let moviesArray = movies.filter(movie => movie.id !== movieID) //i think thats what we need
+    if (moviesArray) {
+        fs.writeFile('movies.json', JSON.stringify(moviesArray), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        })
+        res.status(200).send(`Deleting movie with ID ${movieID}`)
+    } else {
+        res.status(404).send('No movies found')
+    }
+})//working
 
 module.exports = router;
